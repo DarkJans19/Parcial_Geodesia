07ac2494c73a5bb87477fd87a836aec7c8a32110
DarkJans19 <jansito4221@gmail.com>
2024-10-07 16:05:41 -0500

    Fixed the problems and added so much prints for debugging i need erase them then

--- Punto_1/Longitudes de arco/SistemaWGS84.py
+++ Punto_1/Longitudes de arco/SistemaWGS84.py
@@ -20,3 +20,55 @@ class SistemaWGS84:
         longitud = numerador/division
         longitud_arco_paralelo = longitud * delta_x 
         return longitud_arco_paralelo
+    
+
+    """
+    Toda la formula es: 
+    Tomando y como los angulos de los paralelos
+    a(y1 - y2) - (1-e²)a/2 * sin(2(y1+y2)) * (y2-y1)
+    """
+    
+    @staticmethod
+    def calcular_longitud_arco_meridiano(delta_y: float, suma_y: float, x: float, e: float):
+        parte_1 = SistemaWGS84.a * (delta_y)
+        parte_2 = ((1 - e **2) * SistemaWGS84.a)/2
+        arg_sen = 2 * suma_y
+        parte_3 = math.sin(arg_sen)
+        parte_4 = parte_2 * parte_3 * delta_y
+        longitud_arco_meridiano = parte_1 - parte_4
+        return longitud_arco_meridiano
+    
+    """ 
+    Tomando la formula del cuadrilatero suponiendo que 
+    AD = BC
+    Y que AB = CD = L
+    """
+    
+    def calcular_valor_interno_area(latitud, e):
+        ajuste_excentricidad = 1 - (pow(e, 2) * pow(math.sin(latitud), 2))
+        sen_angulo = math.sin(latitud)
+        print("Sen_angulo", sen_angulo)
+        print("ajuste_excentricidad:", ajuste_excentricidad)
+        parte_1 = sen_angulo/(2 * ajuste_excentricidad)
+        print("parte_1", parte_1)
+        
+        # Parte 2
+        parte_2_numerador = (e * sen_angulo) + 1
+        print("parte_2_numerador: ", parte_2_numerador)
+        parte_2_denominador = abs((e * sen_angulo) - 1)
+        print("Parte_2_denominador:", parte_2_denominador) 
+        parte_2 = parte_2_numerador/parte_2_denominador
+        print("parte_2", parte_2)
+        logaritmo = math.log(parte_2)
+        print("logaritmo", logaritmo)
+        parte_2 = logaritmo / (4 * e)
+        print("parte_2:", parte_2)
+        return parte_1 + parte_2
+    
+    def calcular_cuadrilatero(delta_x, area_1, area_2):
+        parte_1 = pow(SistemaWGS84.b, 2) * delta_x    
+        print(parte_1)
+        area_total = area_1 - area_2
+        print("area total: ", area_total)
+        area_cuadrilatero = parte_1 * area_total
+        return area_cuadrilatero
--- Punto_1/Longitudes de arco/main.py
+++ Punto_1/Longitudes de arco/main.py
@@ -1,4 +1,3 @@
-import math
 from CoordenadaGMS import CoordenadaGMS
 from punto import Punto
 from SistemaWGS84 import SistemaWGS84
@@ -13,25 +12,74 @@ def main():
     print(f"Las constantes del sistema son {a} y {b} y la excentricidad es {e}")
     
     # Coordenadas x de ambos puntos
-    punto_a_x = CoordenadaGMS(80, 20, 0, 'E')
+    punto_a_x = CoordenadaGMS(80,20, 0, 'E')
     punto_b_x = CoordenadaGMS(95, 15, 0, 'E')
+    punto_c_x = CoordenadaGMS(80, 20, 0, 'E')
+    punto_d_x = CoordenadaGMS(95, 15, 0, 'E')
 
     # Coordenadas y de ambos puntos
-    punto_a_y = CoordenadaGMS(10, 25, 0, 'S')
-    punto_b_y = CoordenadaGMS(10, 25, 0, 'S')
+    punto_a_y = CoordenadaGMS(20, 30, 0, 'N')
+    punto_b_y = CoordenadaGMS(20, 30, 0, 'N')
+    punto_c_y = CoordenadaGMS(10, 25, 0, 'S')
+    punto_d_y = CoordenadaGMS(10, 25, 0, 'S')
     
-    # Juntamos ambas coordenadas 
+    # Juntamos todas las coordenadas 
     punto_1 = Punto(punto_a_x, punto_a_y)
     punto_2 = Punto(punto_b_x, punto_b_y)
+    punto_3 = Punto(punto_c_x, punto_c_y)
+    punto_4 = Punto(punto_d_x, punto_d_y)
+    
+    print(punto_1)
+    print(punto_2)
+    print(punto_3)
+    print(punto_4)
     
     # Convertimos las coordenadas y de los puntos a radianes
+    punto_a_x = punto_a_x.convertir_a_radianes()
     punto_a_y = punto_a_y.convertir_a_radianes()
+    punto_b_x = punto_b_x.convertir_a_radianes()
+    punto_b_y = punto_b_y.convertir_a_radianes()
+    punto_c_x = punto_c_x.convertir_a_radianes()
+    punto_c_y = punto_c_y.convertir_a_radianes()
+    punto_d_x = punto_d_x.convertir_a_radianes()
+    punto_d_y = punto_d_y.convertir_a_radianes()
+    
+    # Podemos convertir las coordenadas este oeste y las usamos para calcular el ejercicio
+    latitud_1 = punto_a_y
+    latitud_2 = punto_c_y
+    
+    longitud_1 = punto_a_x
+    longitud_2 = punto_b_x
     
     # Calculamos la diferencia de coordenadas x entre los puntos
-    delta_x = punto_2.x.diferencia_coordenada_x(punto_1.x)
-
-    longitud_arco_paralelo = SistemaWGS84.calcular_longitud_arco_paralelo(delta_x, punto_a_y,e)
-    print("Longitud_arco_paralelo:", longitud_arco_paralelo)
+    # Para calcular la longitud del arco paralelo ir alternando entre el punto mayor y el punto menor
+    print("latitud_1:", latitud_1)
+    print("latitud_2:",latitud_2)
+    print("longitud_1:", longitud_1)
+    print("longitud_2:", longitud_2)
+    
+    delta_x = punto_4.x.diferencia_coordenada(punto_1.x)
+    print("delta_x:", delta_x)
+    
+    delta_y = punto_1.y.diferencia_coordenada(punto_4.y)
+    print("delta_y:",delta_y)
+    
+    suma_y = punto_1.y.suma_coordenada(punto_4.y)
+    print("suma_y:",suma_y)
+    
+    # Podemos calcular la longitud del arco de paralelo y meridiano entre dos puntos
+    longitud_arco_paralelo = SistemaWGS84.calcular_longitud_arco_paralelo(delta_x, latitud_1,e)
+    print("Longitud arco paralelo:", longitud_arco_paralelo)
+    longitud_arco_meridiano = SistemaWGS84.calcular_longitud_arco_meridiano(delta_y, suma_y, longitud_1, e)
+    print("Longitud de arco meridiano:", longitud_arco_meridiano)
+    
+    # Calculamos el area del cuadrilatero
+    area_1 = SistemaWGS84.calcular_valor_interno_area(abs(latitud_1), e)
+    print("area 1:", area_1)
+    area_2 = SistemaWGS84.calcular_valor_interno_area(abs(latitud_2), e)
+    print("area_2:", area_2)
+    area_cuadrilatero = SistemaWGS84.calcular_cuadrilatero(delta_x, area_1, area_2)
+    print("Area del cuadrilátero:", area_cuadrilatero)
     
 if __name__ == "__main__":
     main()
